8d71c290cb9440fda7370dec8567f82a03c4e514
ohhepicfail <ohh.epicfail@gmail.com>
2016-10-13 20:10:50 +0300

    some refactoring

--- tree.h
+++ tree.h
@@ -5,13 +5,13 @@ namespace tree {
 
 	struct tree_t;
 
-	tree_t* create_tree     (tree_t* parent, int data);
-	void    delete_tree     (tree_t* tree);
-	void	add			    (tree_t* tree, int data);
-	void	print_tree	    (const tree_t* tree);
-	void	printin_dot     (const tree_t* tree);
-	void	get_2addresses  (const tree_t* tree, const tree_t** fst, const tree_t** snd);
-	int 	calculate_lengh (const tree_t* fst, const tree_t* snd);
+	tree_t* create_tree      (tree_t* parent, int data);
+	void    delete_tree      (tree_t* tree);
+	void	add			     (tree_t* tree, int data);
+	void	print_tree	     (const tree_t* tree);
+	void	printin_dot      (const tree_t* tree);
+	void	get_2addresses   (const tree_t* tree, const tree_t** fst, const tree_t** snd);
+	int 	calculate_length (const tree_t* fst, const tree_t* snd);
 }
 
 #endif

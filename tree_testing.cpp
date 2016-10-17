9dbceff710eaa1eda60a466f740bf29e22299229
ohhepicfail <ohh.epicfail@gmail.com>
2016-10-17 23:17:25 +0300

    new tests

--- tree_testing.cpp
+++ tree_testing.cpp
@@ -3,32 +3,42 @@
 #include <cstdio>
 #include "tree.h"
 
+#define PRINT_TREE 0
+
 using namespace tree;
 
 int main () {
 
 
 	tree_t* tree1 = create_tree (nullptr, 5);
-
 	srand (time (nullptr));
-	int tree_size = 150;
+	int tree_size = 1501;
 	for (int i = 1; i < tree_size; i++) {
 		add (tree1, rand () % tree_size);
 	}
-
-	printin_dot (tree1);
 
 	const tree_t* fst1 = nullptr;
 	const tree_t* snd1 = nullptr;
 	get_2addresses (tree1, &fst1, &snd1);
 
-	print_tree (fst1);
-	printf ("\n\n\n");
-	print_tree (snd1);
-	printf ("\n\n\n");
+	#if PRINT_TREE
+		printin_dot (tree1);
+		print_tree (fst1);
+		printf ("\n\n\n");
+		print_tree (snd1);
+		printf ("\n\n\n");
+	#endif
 
-	printf ("way lengh %d\n", calculate_length (fst1, snd1));
+	std::clock_t start;
+	start = std::clock();
+	printf ("1) way lengh  %d\n", calculate_length (fst1, snd1));
+	double duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("calculate_length time %lf\n", duration);
 
+	start = std::clock();
+	printf ("2) way lengh  %d\n", length_bitlabel (fst1, snd1));
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("length_bitlabel  time %lf\n", duration);
 
 	tree_t* tree2 = create_tree (nullptr, 5);
 	for (int i = 1; i < tree_size; i++) {
@@ -38,7 +48,15 @@ int main () {
 	const tree_t* snd2 = nullptr;
 	get_2addresses (tree2, &fst2, &snd2);
 
-	printf ("way lengh %d\n", calculate_length (fst1, snd2));
+	start = std::clock();
+	printf ("1) way lengh  %d\n", calculate_length (fst1, snd2));
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("calculate_length time %lf\n", duration);
+
+	start = std::clock();
+	printf ("2) way lengh  %d\n", length_bitlabel (fst1, snd2));
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("length_bitlabel  time %lf\n", duration);
 
 	delete_tree (tree1);
 	delete_tree (tree2);

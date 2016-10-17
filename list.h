2128931f6a08bfbd1c9d9940ecffec1de40ffad0
ohhepicfail <ohh.epicfail@gmail.com>
2016-10-17 22:25:33 +0300

    added new finding loop function and fixed some warnings

--- list.h
+++ list.h
@@ -5,16 +5,17 @@ namespace list {
 
 	struct node_t;
 
-	void 	 	delete_list (node_t* list);
-	node_t*	 	create_list (const void* data);
-	void 	 	push_back   (node_t* list, const void* data);
-	void	 	push_front	(node_t** list, const void* data);
-	const void* pop_front	(node_t** list);
-	bool		is_empty	(const node_t* list);
-	void	 	print_list  (const node_t* list);
-	node_t*  	reverse	 	(node_t* list);
-	void	 	random_loop (node_t* list);
-	unsigned 	find_loop	(const node_t* list, const node_t** loop_begin);
+	void 	 	delete_list 		 (node_t* list);
+	node_t* 	create_or_push_front (node_t* list, const void* data);
+	void 	 	push_back   		 (node_t* list, const void* data);
+	const void* pop_front			 (node_t** list);
+	bool		is_empty			 (const node_t* list);
+	void	 	print_list  		 (const node_t* list);
+	node_t*  	reverse	 			 (node_t* list);
+	void	 	random_loop 		 (node_t* list);
+	unsigned 	find_loop			 (const node_t* list, const node_t** loop_begin);
+	node_t*		copy				 (const node_t* list);
+	unsigned	find_loop_bitlabel	 (const node_t* list, const node_t** loop_begin);
 }
 
 

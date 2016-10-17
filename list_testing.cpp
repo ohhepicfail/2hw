c7745809cc2af5c73e5005509ceb9a6816b8edf5
ohhepicfail <ohh.epicfail@gmail.com>
2016-10-17 22:26:26 +0300

    rewrote tests

--- list_testing.cpp
+++ list_testing.cpp
@@ -1,22 +1,48 @@
 #include <cstdio>
+#include <ctime>
 #include "list.h"
 
 using namespace list;
 
 int main () {
-	node_t* list = create_list (0);
+	node_t* list = create_or_push_front (nullptr, 0);
 
-	for (unsigned i = 0; i < 6u; i++)
-		push_back (list, static_cast <const void*> (&i - i));
+	for (unsigned i = 0; i < 150000000u; i++)
+		list = create_or_push_front (list, static_cast <const void*> (&i - i));
 
-	print_list (list);
 	random_loop (list);
 
-	unsigned loop_size = find_loop (list, nullptr);
-	printf ("loop size %u\n", loop_size);
+	const node_t* lb    = nullptr;
+	const node_t* lb_bl = nullptr;
+
+	std::clock_t start;
+	start = std::clock();
+	unsigned loop_size    = find_loop (list, &lb);
+	double duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("find_loop time %lf\n", duration);
+
+	start = std::clock ();
+	unsigned loop_size_bl = find_loop_bitlabel (list, &lb_bl);
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("find_loop_bitlabel time %lf\n", duration);
+
+	printf ("%d\t%d\n", loop_size == loop_size_bl, lb == lb_bl);
+
 	list = reverse (list);
-	printf ("after reversing:\n");
-	print_list (list);
+	lb    = nullptr;
+	lb_bl = nullptr;
+
+	start = std::clock ();
+	loop_size    = find_loop (list, &lb);
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("find_loop time %lf\n", duration);
+
+	start = std::clock ();
+	loop_size_bl = find_loop_bitlabel (list, &lb_bl);
+	duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+	printf ("find_loop_bitlabel time %lf\n", duration);
+
+	printf ("%d\t%d\n", loop_size == loop_size_bl, lb == lb_bl);
 
 	delete_list (list);
 }

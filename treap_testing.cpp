a69b07663b7e389da3af51a33a5a9995471739fa
ohhepicfail <ohh.epicfail@gmail.com>
2016-10-23 02:53:20 +0300

    code refactoring

--- treap_testing.cpp
+++ treap_testing.cpp
@@ -9,12 +9,12 @@
 using namespace treap;
 
 int main () {
-	const unsigned n = 3000000;
+	const unsigned n = 1000000;
 	int* data = new int[n];
 
 	srand (time (nullptr));
 	for (unsigned i = 0; i < n; i++)
-		data[i] = rand () % n + 1;
+		data[i] = rand () % n;
 
 	treap_t* treap = create_treap (data, n);
 
@@ -44,45 +44,3 @@ int main () {
 
 	return 0;
 }
-
-// int main () {
-// 	const unsigned n = 1000000;
-// 	int* data = new int[n];
-
-// 	srand (time (nullptr));
-// 	for (unsigned i = 0; i < n; i++)
-// 		data[i] = rand ();
-
-// 	treap_t* treap = create_treap (data, n);
-
-// 	#if PRINT
-// 		for (unsigned i = 0; i < n; i++)
-// 			printf ("%d\t", data[i]);
-// 		printin_dot (treap);
-// 	#endif
-
-// 	printf ("begin\n");
-// 	for (unsigned i = 0; i < 100000; i++) {
-// 		unsigned fst = rand () % n;
-// 		unsigned snd = rand () % n;
-// 		if (fst > snd) {
-// 			unsigned tmp = fst;
-// 			fst = snd;
-// 			snd = tmp;
-// 		}
-
-// 		#if PRINT
-// 			printf ("\nfst = %u\t snd = %u\n%d\t", fst, snd, find_min (treap, fst, snd));
-// 		#endif
-
-// 		int min = find_min (treap, fst, snd);
-// 		for (; i % 1000 == 0 && fst <= snd; fst++)
-// 			assert (data[fst] >= min);
-// 	}
-
-// 	printf ("end\n");
-// 	delete[] data;
-// 	delete_treap (treap);
-
-// 	return 0;
-// }

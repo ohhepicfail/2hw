7281e07dd2517fea6c1f8d9f5d9c2e634452dee8
ohhepicfail <ohh.epicfail@gmail.com>
2016-10-23 00:28:06 +0300

    added Tarjan's algorithm drafts

--- treap.cpp
+++ treap.cpp
@@ -11,14 +11,21 @@ namespace treap {
 		treap_t* parent_;
 		treap_t* left_;
 		treap_t* right_;
-
 		int data_;
 		unsigned idx_;
+
+		treap_t* lcaparent_;
+		unsigned rank_;
 	};
 
 
-	static inline void		write_dot_file		(FILE* file, const treap_t* treap);
-	static inline treap_t*	get_vert_from_idx	(treap_t* treap, unsigned idx);
+	static inline void				write_dot_file		(FILE* file, const treap_t* treap);
+	static inline treap_t*			get_vert_from_idx	(treap_t* treap, unsigned idx);
+	static 		  void				dfs_olca	   		(treap_t* treap, unsigned* fst, unsigned* snd, int* answers, unsigned size, int* lca);
+	static 		  void 				dsf_make_set		(treap_t* vert);
+	static 		  void 				dsf_union			(treap_t* v1, treap_t* v2);
+	static  	  treap_t*			dsf_find			(treap_t* vert);
+
 
 
 	treap_t* create_treap (const int* data, unsigned size) {
@@ -28,15 +35,19 @@ namespace treap {
 		treap_t* prev = new treap_t;
 		prev->data_ = data[0];
 		prev->idx_  = 0;
-		prev->parent_ = nullptr;
-		prev->left_   = nullptr;
-		prev->right_  = nullptr;
+		prev->rank_ = 0;
+		prev->parent_ 	 = nullptr;
+		prev->left_   	 = nullptr;
+		prev->right_  	 = nullptr;
+		prev->lcaparent_ = nullptr;
 		for (unsigned i = 1; i < size; i++) {
 			treap_t* vertex = new treap_t;
 			vertex->data_ = data[i];
 			vertex->idx_  = i;
-			vertex->left_  = nullptr;
-			vertex->right_ = nullptr;
+			vertex->rank_ = 0;
+			vertex->left_  	   = nullptr;
+			vertex->right_ 	   = nullptr;
+			vertex->lcaparent_ = nullptr;
 
 			if (data[i] >= prev->data_) {
 				prev->right_ = vertex;
@@ -173,5 +184,119 @@ namespace treap {
 		}
 
 		return treap;
+	}
+
+
+	void tarjan_olca (treap_t* treap, unsigned* fst, unsigned* snd, int* answers, unsigned size) {
+		assert (treap);
+		assert (fst);
+		assert (snd);
+		assert (answers);
+
+		// todo: sort_requests (fst, snd);
+		for (unsigned i = 0; i < size; i++) {
+			if (fst[i] < snd[i]) {
+				unsigned tmp = fst[i];
+				fst[i] = snd[i];
+				snd[i] = tmp;
+			}
+		}
+
+		int* lca = new int[size];
+		dfs_olca (treap, fst, snd, answers, size, lca);
+
+		printin_dot (treap);
+
+		for (unsigned i = 0; i < size; i++) {
+			// printf ("idxs: %u -- %u\ttarjan olca %d\n",fst[i], snd[i], answers[i]);
+			int min = find_min (treap, fst[i], snd[i]);
+			// printf ("treap lca %d\n", min); 
+			assert (min == answers[i]);
+		}
+
+		// todo: check and print
+
+		delete[] lca;
+
+	}
+
+
+	void dfs_olca (treap_t* treap, unsigned* fst, unsigned* snd, int* answers, unsigned size, int* lca) {
+		assert (treap);
+		assert (fst);
+		assert (snd);
+		assert (answers);
+		assert (lca);
+
+		dsf_make_set (treap);
+		lca[treap->idx_] = treap->data_;
+		#define DFS_OLCA( x )	dfs_olca (x, fst, snd, answers, size, lca)
+		if (treap->left_) {
+			DFS_OLCA (treap->left_);
+			dsf_union (treap, treap->left_);
+			lca[dsf_find (treap)->idx_] = treap->data_;
+		}
+		if (treap->right_) {
+			DFS_OLCA (treap->right_);
+			dsf_union (treap, treap->right_);
+			lca[dsf_find (treap)->idx_] = treap->data_;
+		}
+		#undef DFS_OLCA
+
+		for (unsigned i = 0; i < size; i++)
+			if (fst[i] == treap->idx_) {
+				// printf ("saving answers vertex_idx - %u\tidxs %u -- %u\n\t", treap->idx_, fst[i], snd[i]);
+				// printf ("fst");
+				// for (unsigned i = 0; i < size; i++)
+				// 	printf ("  %u", fst[i]);
+				// printf ("\n\tsnd");
+				// for (unsigned i = 0; i < size; i++)
+				// 	printf ("  %u", snd[i]);
+				// printf ("\n\tlca");
+				// for (unsigned i = 0; i < size; i++)
+				// 	printf ("  %d", lca[i]);
+				// printf ("\n");
+				treap_t* vert = get_vert_from_idx (treap, snd[i]);
+				assert (vert);
+				// printf ("\tvert idx %u\n\n", vert->idx_); 
+				answers[i] = lca[dsf_find (vert)->idx_];
+
+			}
+	}
+
+
+	static void dsf_make_set (treap_t* vert) {
+		assert (vert);
+
+		vert->lcaparent_ = vert;
+		vert->rank_ = 0;
+	}
+
+
+	static void dsf_union (treap_t* v1, treap_t* v2) {
+		assert (v1);
+		assert (v2);
+
+		treap_t* v1_root = dsf_find (v1);
+		treap_t* v2_root = dsf_find (v2);
+
+		if 		(v1_root->rank_ > v2_root->rank_)
+			v2_root->lcaparent_ = v1_root;
+		else if (v1_root->rank_ < v2_root->rank_)
+			v1_root->lcaparent_ = v2_root;
+		else {
+			v2_root->lcaparent_ = v1_root;
+			v1_root->rank_++;
+		}
+	}
+
+
+	static treap_t* dsf_find (treap_t* vert) {
+		assert (vert);
+
+		if (vert->lcaparent_ != vert)
+			vert->lcaparent_ = dsf_find (vert->lcaparent_);
+
+		return vert->lcaparent_;
 	}
 }
--- treap.h
+++ treap.h
@@ -5,10 +5,11 @@ namespace treap {
 
 	struct treap_t;
 
-	treap_t* create_treap (const int* data, unsigned size);
-	void 	 printin_dot  (const treap_t* treap);
-	void 	 delete_treap (treap_t* treap);
-	int		 find_min	  (treap_t* treap, unsigned fst, unsigned snd);
+	treap_t* create_treap  (const int* data, unsigned size);
+	void 	 printin_dot   (const treap_t* treap);
+	void 	 delete_treap  (treap_t* treap);
+	int		 find_min	   (treap_t* treap, unsigned fst, unsigned snd);
+	void	 tarjan_olca   (treap_t* treap, unsigned* fst, unsigned* snd, int* answers, unsigned size);
 }
 
 #endif

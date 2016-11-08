ec5b3990fad6d08069e47a4928ad7950c6945d04
ohhepicfail <ohh.epicfail@gmail.com>
2016-11-08 19:23:50 +0300

    simple test

--- ukk/ukk_test.cpp
+++ ukk/ukk_test.cpp
@@ -9,49 +9,22 @@ using namespace suffix_tree;
 using namespace str_proc;
 
 
-void get_rand_str (char* str, unsigned length) {
-	assert (str);
-
-	for (unsigned i = 0; i < length - 1; i++)
-		str[i] = rand () % 21 + 'a';
-
-	str[length - 1] = '\0';
-}
-
-#define FIND_STR  0
-
 int main () {
-    vertex* suffix_tree = nullptr;
+    char fname[] = "strings.txt";
+    char* text = get_text_for_ukk (fname);
+    printf ("\n\ntext is ready\n\n");
 
-    #if FIND_STR
-    srand (time (nullptr));
-    unsigned length = 1000;
-    char* str = new char[length];
-    for (unsigned i = 0; i < 100000; i++)
-    {
-    	get_rand_str (str, length);
-    	// printf ("%s\n", str);
-    	suffix_tree = build (str);
-    	unsigned chb,che;
-    	bool is_ok = test (suffix_tree, str, &chb, &che);
-    	clean (suffix_tree);
-    	if (!is_ok) {
-    		printf ("%s\n", str);
-    		return -1;
-    	}
+    vertex* suffix_tree = build (text);
+    printf ("suffix tree created!\n\n");
 
-    }
-    #else
-	    char fname[] = "strings.txt";
-	    char* text = get_text_for_ukk (fname);
-	    suffix_tree = build (text);
-	    // print (suffix_tree, text);
-	    unsigned chb,che;
-	    bool is_ok = test (suffix_tree, text, &chb, &che);
-	    if (!is_ok)
-	    	printf ("something go wrong! chb %u %c  che %u %c\n", chb, text[chb], che, text[che]);
-	    clean (suffix_tree);
-	    delete[] text;  
-    #endif  
+    print (suffix_tree, text);
+    bool is_ok = test (suffix_tree, text);
+    if (!is_ok)
+    	printf ("something go wrong!\n");
+    else
+    	printf ("all is ok\n\n");
+
+    clean (suffix_tree);
+    delete[] text;  
     return 0;
 }

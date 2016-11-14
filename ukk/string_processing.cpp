d95e39d4a2743a27803a522439ae4f443964704a
ohhepicfail <ohh.epicfail@gmail.com>
2016-11-14 12:48:54 +0300

    features to build generalized suffix tree

--- ukk/string_processing.cpp
+++ ukk/string_processing.cpp
@@ -9,8 +9,8 @@ namespace str_proc {
 	static inline unsigned  get_file_size           (const char* filename);
     static inline char*     read_file               (const char* filename, unsigned fsize);
     static inline unsigned  count_numbers           (unsigned n);
-    static inline char*     concatenate_strings_v1  (const char* strings, unsigned tsize);
-    static inline char*     concatenate_strings_v2  (const char* strings, unsigned tsize);
+    static inline char*     concatenate_strings_v1  (const char* strings, unsigned tsize, suffix_tree::bounds::str_bounds** sb);
+    static inline char*     concatenate_strings_v2  (const char* strings, unsigned tsize, suffix_tree::bounds::str_bounds** sb);
 
 
     static inline unsigned get_file_size (const char* filename) {
@@ -43,6 +43,8 @@ namespace str_proc {
 
     static inline unsigned count_numbers (unsigned n) {
         unsigned counter = 0;
+        if (n == 0)
+            return 1u;
         while (n > 0) {
             n /= 10;
             counter++;
@@ -51,60 +53,78 @@ namespace str_proc {
         return counter;
     }
 
-    static inline char* concatenate_strings_v1 (const char* strings, unsigned tsize) {
+    static inline char* concatenate_strings_v1 (const char* strings, unsigned tsize, suffix_tree::bounds::str_bounds** sb) {
         assert (strings);
 
-        unsigned extra_symb = 0;
+        unsigned extra_symb = 1u;       // $0 at the end
         for (unsigned i = 0; i < tsize; ++i)
             if (strings[i] == '\n')
                 extra_symb++;
 
-        unsigned extra_size = 0;
-        for (unsigned i = 1; i <= extra_symb; i++)
+        unsigned extra_size = 0;       
+        for (unsigned i = 0; i <= extra_symb; i++)
                 extra_size += count_numbers (i);
 
-        char* text = new char[tsize + extra_size];
+        char* text = new char[tsize + extra_size + extra_symb];
+        unsigned* strings_bounds = new unsigned[extra_symb];
+        unsigned nstr = extra_symb; 
+        strings_bounds[0] = 0;
+        for (unsigned i = 0, j = 0, k = 1; i < tsize; ++i) {
 
-        for (unsigned i = 0, j = 0; i < tsize; ++i) {
+            if (strings[i] == '\n' || strings[i] == '\0') {
 
-            if (strings[i] == '\n') {
-                text[j++] = '\\';
+                text[j++] = '$';
                 strcpy (text + j, std::to_string (extra_symb).c_str ());
                 j += count_numbers (extra_symb);
                 extra_symb--;
-            } 
+                text[j++] = '/';
 
+                if (strings[i] == '\0')
+                    text[j++] = strings[i];
+                else
+                    strings_bounds[k++] = j;
+            } 
             else
                 text[j++] = strings[i];
         }
 
+        *sb = suffix_tree::bounds::set (strings_bounds, nstr);
+
         return text;
     }
 
 
-    static inline char* concatenate_strings_v2 (const char* strings, unsigned tsize) {
+    static inline char* concatenate_strings_v2 (const char* strings, unsigned tsize, suffix_tree::bounds::str_bounds** sb) {
         assert (strings);
 
         unsigned extra_symb = 0;
         for (unsigned i = 0; i < tsize; ++i)
-            if (strings[i] == '\n')
+            if (strings[i] == '\n' && strings[i + 1] != '\0')
                 extra_symb++;
 
         char* text = new char[tsize - extra_symb];
-        for (unsigned i = 0, j = 0; i < tsize; i++)
+        unsigned* strings_bounds = new unsigned[extra_symb + 1]; 
+        for (unsigned i = 0, j = 0, k = 0; i < tsize; i++) {
             if (strings[i] != '\n')
                 text[j++] = strings[i];
+            else
+                strings_bounds[k++] = j;
+        }
+        strings_bounds[extra_symb] = 0;
+
+        *sb = suffix_tree::bounds::set (strings_bounds, extra_symb + 1);
 
         return text;
     }
 
 
-    char* get_text_for_ukk (const char* filename) {
+    char* get_text_for_ukk (const char* filename, suffix_tree::bounds::str_bounds** sb) {
         assert (filename);
         unsigned fsize = get_file_size (filename);
         char* text = read_file (filename, fsize);
 
-        char* text2 = concatenate_strings_v2 (text, fsize);
+        char* text2 = concatenate_strings_v1 (text, fsize, sb);
+        // char* text2 = concatenate_strings_v2 (text, fsize, sb);
 
         delete[] text;
         return text2;
--- ukk/string_processing.h
+++ ukk/string_processing.h
@@ -1,8 +1,10 @@
 #ifndef STRING_PROCESSING_H
 #define STRING_PROCESSING_H
 
+#include "ukk.h"
+
 namespace str_proc {
-	char* get_text_for_ukk (const char* filename);
+	char* get_text_for_ukk (const char* filename, suffix_tree::bounds::str_bounds** sb);
 }
 
 #endif

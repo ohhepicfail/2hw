4c3c0b9e4923cd60b6a632882e15ae485e43af09
ohhepicfail <ohh.epicfail@gmail.com>
2016-11-29 15:10:38 +0300

    demo

--- avl/avl_test.cpp
+++ avl/avl_test.cpp
@@ -8,14 +8,20 @@ int main () {
 	avl::AVL_tree avl (5);
 
 	srand (time (0));
-	auto n = 50;
+	auto n = 20;
 	for (int i = 0; i < n; i++)
-		avl.insert (rand () % n);
+		avl.insert (i);
+
 
 	avl.print ("avl.dot");
-	for (int i = 0; i < n; i++)
-		avl.remove (rand () % n);
-	avl.print ("avl1.dot");
+
+	printf ("number of the elements that is less than\n");
+	for (auto i = -1; i < n + 2; i++)
+		printf ("%d\t%u\n", i, avl.n_elem_lessthan (i));
+
+	printf ("\nn min elem\n");
+	for (unsigned i = 1; i < n + 1; i++)
+		printf ("%u\t%u\n", i, avl.find_nmin (i));
 
 	return 0;
 }
